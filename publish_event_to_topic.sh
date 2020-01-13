24ed4089416055bd13136daad80d20588d9393d1
pablo rodriguez defino <pablo.defino@getcruise.com>
2020-01-13 12:22:13 -0800

    improved code documentation

--- publish_event_to_topic.sh
+++ publish_event_to_topic.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+# sends a dummy workflow execution request
+
 topic_name=$1
 pyspark_script_location=$2
 gcloud pubsub topics publish $1 --message "{\"job_name\":\"dragon\", \"jobs\":[{\"step_id\":\"step1\", \"pyspark_job\": {\"main_python_file_uri\": \"$pyspark_script_location\"}}]}"
--- tf/functions/cf_launcher.py
+++ tf/functions/cf_launcher.py
@@ -16,6 +16,12 @@ from google.cloud import pubsub_v1
 # governed by the terms of that agreement, and the foregoing does not supersede that agreement.
 
 def retrieve_configuration(storage_client):
+    """ Retrieves Cloud Function configuration
+    Using the configured bucket and file, downloads the existing configuration
+    and returns it as a JSON object.
+
+    storage_client: an initialized GCS client
+    """
     config_bucket = '${config_bucket}'
     config_file = '${config_file}'
 
@@ -25,17 +31,29 @@ def retrieve_configuration(storage_client):
     return json.loads(blob.download_as_string())
 
 def result_propagation_callback(future):
+    """ Logs propagation result
+    future: Pubsub publish operations result
+    """
     print("Workflow result Pubsub message notification id {} ".format(future.result()))
 
 def propagate_result(result):
+    """ Workflow results propagation
+    Propagates the execution's result to the configured Pubsub topic.
+
+    result: workflow execution information
+    """
     publisher = pubsub_v1.PublisherClient()
     topic_path = publisher.topic_path('${project}', '${propagate_results_topic}')
     future = publisher.publish(topic_path, data=json.dumps(result).encode("utf-8"))
     future.add_done_callback(result_propagation_callback)
 
-def execution_callback(operation_future, template_name, cluster_name,
-            metadata):
-    # Handle workflow execution result.
+def execution_callback(operation_future, metadata):
+    """ Handle workflow execution result
+    Captures the execution request information, the result as well and propagates it.
+
+    operation_future: the future reference for the execution, fulfilled when execution ends
+    metadata: the workflow request metadata
+    """
     result = str(operation_future.result()) if operation_future.result() is not None or operation_future.result() is not Empty else 'ok'
     metadata['cluster_name'] = operation_future.metadata.cluster_name
     metadata['cluster_uuid'] = operation_future.metadata.cluster_uuid
@@ -49,53 +67,79 @@ def execution_callback(operation_future, template_name, cluster_name,
     propagate_result({'metadata': metadata, 'result': result})
 
 def trigger_dataproc_jobs(message, context):
-    event = None
+    """ Entry point for the CloudFunction
+    Captures a Pubsub message from the configured source topic and constructs a
+    Dataproc Inline Workflow request to run the jobs specified in the message request.
+
+    message: the Pubsub message
+    context: the Cloud Function context information
+
+    """
+
     if not 'data' in message:
         print("no data in the Pubsub message, nothing to do...")
         return
     event = json.loads(base64.b64decode(message['data']).decode('utf-8'))
-    dataproc_client = dataproc_v1.WorkflowTemplateServiceClient()
-    storage_client = storage.Client()
-
-    config = retrieve_configuration(storage_client)
-
-    project = '${project}'
-    region = 'global'
-    parent = dataproc_client.region_path(project, region)
-
-    zone = event.get('zone', 'us-central1-c')
-    job_name = event.get('job_name', 'dataproc-workflow-test')
-    template_name = "projects/{}/regions/{}/workflowTemplates/{}".format(project, region, job_name)
-    cluster_name = 'cluster-' + job_name
-    config['cluster_config']['cluster_name'] = cluster_name
-
-    bucket = '${script_bucket}'
-    cluster_init_actions = event.get('cluster_init_actions', [])
-
-    if not isinstance(cluster_init_actions, list):
-        print("cluster initialization actions should be a list")
-        return
 
     if not "jobs" in event.keys():
         print("jobs property not present in the event, no work to be done...")
         return
 
-    template = {
+    # initialize needed GCP clients
+    dataproc_client = dataproc_v1.WorkflowTemplateServiceClient()
+    storage_client = storage.Client()
+
+    # retrieves the cloud function configuration for storage
+    config = retrieve_configuration(storage_client)
+
+    # build parent region path for dataproc api requests
+    project = '${project}'
+    region = 'global'
+    parent = dataproc_client.region_path(project, region)
+
+    # extract events parameters
+    zone = event.get('zone', 'us-central1-c')
+    job_name = event.get('job_name', 'dataproc-workflow-test')
+    template_name = "projects/{}/regions/{}/workflowTemplates/{}".format(project, region, job_name)
+    cluster_name = 'cluster-' + job_name
+    cluster_init_actions = event.get('cluster_init_actions', [])
+    if not isinstance(cluster_init_actions, list):
+        print("cluster initialization actions should be a list")
+        return
+
+    # check on the functions configuration for an entry for the job name in the execution request
+    cluster_config = None
+    if not job_name in config.keys():
+        # if no particular configuration exists use default one
+        cluster_config = config['default_cluster_config']
+    else:
+        cluster_config = config[job_name]
+
+    cluster_config['cluster_name'] = cluster_name
+    cluster_config['config']['initialization_actions'] = cluster_init_actions
+
+    # creates inline template request
+    inline_template = {
       'name': template_name,
-      'version': 1,
       'placement': {
-          'managed_cluster': config['cluster_config'],
+          'managed_cluster': cluster_config
       },
       'jobs': event['jobs']
     }
 
-    response = dataproc_client.instantiate_inline_workflow_template(parent, template)
+    response = dataproc_client.instantiate_inline_workflow_template(parent, inline_template)
 
+    # captures operation name for the execution's metadata along with other request parameters
     metadata = {
-        'operation_name': response.operation.name
+        'operation_name': response.operation.name,
+        'template_name': template_name,
+        'cluster_name': cluster_name
     }
 
     print('workflow instance created, operation\'s name: {}'.format(metadata['operation_name']))
 
-    response.add_done_callback(partial(execution_callback, template_name=template_name,
-            cluster_name=cluster_name, metadata=metadata))
+    # Sets the future to be called when the workflow execution completes.
+    # This partial function gets populated with local information, normally
+    # not present at the callback execution time, to enrich logging and event
+    # results propagation.
+    response.add_done_callback(partial(execution_callback, metadata=metadata))
